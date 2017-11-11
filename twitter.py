c68b27218b049342652945cea32f645c619f9e60
nve3pd <uchihara1229@gmail.com>
2017-11-11 16:48:50 +0900

    pep8 ;)

--- twitter.py
+++ twitter.py
@@ -19,7 +19,8 @@ class Twitter:
         return auth
 
     def get_comment(self, hash_tag):
-        req = requests.get(self.URL, auth=self.auth, params={"q": "#"+hash_tag, "count": "100"})
+        params = {"q": "#"+hash_tag, "count": "100"}
+        req = requests.get(self.URL, auth=self.auth, params=params)
         for i in req.iter_lines():
             data = json.loads(i)
             json_data = self.create_json(data, len(data["statuses"]))
@@ -29,10 +30,10 @@ class Twitter:
     def create_json(self, data, length):
         res = {"comment": [{} for i in range(length)]}
         for i in range(length):
-            text = data["statuses"][i]["text"].split()[1]  # tweetの本文
+            pic = data["statuses"][i]["entities"]  # ここからpicのurlを
             date = data["statuses"][i]["created_at"]  # 日付
-            pic = "hoge"  # 画像の
-#            print(data["statuses"][j]["entities"])  # ここからpicのurlを
+            text = data["statuses"][i]["text"].split()[1]  # tweetの本文
+            print(pic, text)
 
             res["comment"][i]["text"] = text
             res["comment"][i]["date"] = date
@@ -42,4 +43,4 @@ class Twitter:
 
 if __name__ == "__main__":
     tw = Twitter()
-    tw.get_comment("#hogepoyo")
+    tw.get_comment("#遊べる掲示板")
